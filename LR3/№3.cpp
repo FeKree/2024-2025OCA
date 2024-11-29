3638570bf3a39f780df176ebf4b1a7c3e30c629d
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-29 16:18:41 +0300

    3 Задание, 14 Вариант

--- LR3/№3.cpp
+++ LR3/№3.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+#include <math.h>
+#include <cmath>
+using namespace std;
 
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int a = 0;
+    cout << "Введите количество коров: ";
+    cin >> a;
+    if (a < 100)
+  {
+    if (a % 10 == 1)
+    {
+        cout << "На лугу пасется " << a << " корова.";
+    }
+    else if (a % 10 == 2 || a % 10 == 3 || a % 10 == 4)
+    {
+        cout << "На лугу пасется " << a << " коровы.";
+    }
+    else
+    {
+        cout << "На лугу пасется " << a << " коров.";
+    }
+  }
+    return 0;
+}
