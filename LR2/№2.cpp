5b667a53a2fb803aa73297dc6481049fd1369de2
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-29 15:46:43 +0300

    2 Задание, 14 Вариант

--- LR2/№2.cpp
+++ LR2/№2.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double u, x, y, z;
+    cout << "Введите x: ";
+    cin >> x;
+    cout << "Введите y: ";
+    cin >> y;
+    cout << "Введите z: ";
+    cin >> z;
+    u = max(y, z) / min(min(x, y), min(y, z));
+    cout << "Результат: " << u;
+    return 0;
+}
