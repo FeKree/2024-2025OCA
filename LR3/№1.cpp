34517c8352d144232815f349f534101445d0b160
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-29 16:16:21 +0300

    1 Задание, 14 Вариант

--- LR3/№1.cpp
+++ LR3/№1.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
 
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    double a, b, c, D, x1, x2;
+        cout << "Введите a: ";
+        cin >> a;
+        cout << "Введите b: ";
+        cin >> b;
+        cout << "Введите c: ";
+        cin >> c;
+        D = pow(b, 2) - 4 * a * c;
+        if (D > 0)
+        {
+            cout << "У уравнеиия будет 2 корня" << endl;
+            x1 = (-1 * b - sqrt(D)) / 2 * a;
+            x2 = (-1 * b + sqrt(D)) / 2 * a;
+            cout << "x1 = " << x1 << endl;
+            cout << "x2 = " << x2 << endl;
+        }
+        else if (D < 0)
+        {
+            cout << "У уравнеиия нет корней" << endl;
+        }
+        else if (D == 0)
+        {
+            cout << "У уравнеиия будет 1 корень" << endl;
+            x1 = -1 * b * 2 * a;
+            cout << "x = " << x1 << endl;
+        }
+
+    return 0;
+}
