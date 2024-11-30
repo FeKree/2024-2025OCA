018f254703c7ffbba9f86b412c9a7b22beb743a2
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-30 14:55:34 +0300

    1 Задание

--- LR9/№1.cpp
+++ LR9/№1.cpp
@@ -1,1 +1,39 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int mm[3][3], n = 3;
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << "Введите значение " << i + 1 << " строки " << j + 1 << " элемента" << endl;
+            cin >> mm[i][j];
+            if (i == 0 && mm[i][j] == 0)
+            {
+                n--;
+            }
+            if (i == 1 && mm[i][j] == 0)
+            {
+                n--;
+            }
+            if (i == 2 && mm[i][j] == 0)
+            {
+                n--;
+            }
+        }
+        
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << mm[i][j] << " ";
+        }
+        cout << endl;
 
+    }
+    cout << n;
+    return 0;
+}
