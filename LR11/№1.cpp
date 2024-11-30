ee4fb8a0e2e3f76f99adf9b29a66ce8ae5ea7225
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-30 15:01:18 +0300

    1 Задание

--- LR11/№1.cpp
+++ LR11/№1.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+#include <cmath>
+#include <string.h>
+using namespace std;
+int main()
+{ 
+    setlocale(LC_ALL, "ru");
+    int count = 0;
+    string mas[] = { "ывы", "почемц", "ллвыл" };
+    for (int i = 0; i < 3; i++)
+    {
+        if (mas[i][0] == mas[i].back())
+        {
+            count++;
+        }
+    }
+    cout << count;
+    return 0;
+}
