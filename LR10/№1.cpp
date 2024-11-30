577e7679b9b5fc68d13bf7adca3b53c562dff858
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-30 14:59:23 +0300

    1 Задание

--- LR10/№1.cpp
+++ LR10/№1.cpp
@@ -1,1 +1,14 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+void cil(double R, double H)
+{
+    double pi = 3.141592;
+    double pcil = pi * R * R * H;
+    cout << "V = " << pcil;
+}
+int main()
+{
+    cil(1, 4);
+    return 0;
+}
