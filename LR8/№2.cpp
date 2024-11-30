fed92c89fd688b9b87a77f6fa4612309a207fe20
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-30 14:52:20 +0300

    2 Задание

--- LR8/№2.cpp
+++ LR8/№2.cpp
@@ -1,1 +1,29 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    int mm[10], p = 0;
+    for (int n = 0; n < 10; n++)
+    {
+        cout << "Введите " << n + 1 << " элемент массива" << endl;
+        cin >> mm[n];
+    }
+    for (int i = 0, n = 9; i < 5, n > 4; i++, n--)
+    {
+        if (mm[i] == mm[n])
+        {
+            p++;
+        }
+    }
+    if (p == 5)
+    {
+        cout << "Это паллиндром";
+    }
+    else
+    {
+        cout << "Это  не паллиндром";
+    }
+    return 0;
+}
