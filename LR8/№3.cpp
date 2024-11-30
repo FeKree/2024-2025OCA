f1418223d401ca8c0f577b9cb4b948743604e4ac
0fekree0 <70841845+FeKree@users.noreply.github.com>
2024-11-30 14:52:55 +0300

    3 Задание

--- LR8/№3.cpp
+++ LR8/№3.cpp
@@ -1,1 +1,35 @@
-
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    setlocale(LC_ALL, "ru");
+    srand(time(NULL));
+    int mm[20], pp[20], nn[20], nnsum = 0, nnmin = 31, nnmax = 9;
+    for (int n = 0; n < 20; n++)
+    {
+        ё
+        pp[n] = rand() % (20 - 10 + 1) + 10;
+    }
+    for (int n = 0; n < 20; n++)
+    {
+        nn[n] = mm[n] + pp[n];
+        
+    }
+    for (int n = 0; n < 20; n++)
+    {
+        if (nn[n] > nnmax)
+        {
+            nnmax = nn[n];
+        }
+        if (nn[n] < nnmin)
+        {
+            nnmin = nn[n];
+        }
+        nnsum += nn[n];
+    }
+    cout << "среднее арифметическое элементов третьего массива " << nnsum / 20 << endl;
+    cout << "Минимальный элемент третьего массива " << nnmin << endl;
+    cout << "Максимлаьный элемент третьего массива " << nnmax << endl;
+    return 0;
+}
